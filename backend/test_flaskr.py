7e484a856a0833f6511b328b5c316539538dc690
Shir Bar Lev <shiruchii@gmail.com>
2020-03-29 13:58:16 +0300

    Added category page tests.

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -64,6 +64,25 @@ class TriviaTestCase(unittest.TestCase):
         self.assertFalse(res_data['success'])
         self.assertFalse(len(res_data['questions']))
 
+    # Test for a category questions pages
+    def get_category_page(self):
+        response = self.clien().get('/categories/1/questions')
+        res_data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(res_data['category'], 1)
+        self.assertTrue(len(res_data['questions']))
+        self.assertTrue(res_data['success'])
+
+    # Test for an out-of-bounds category questions pages
+    def get_out_of_bounds_category(self):
+        response = self.client().get('/categories/100/questions')
+        res_data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertFalse(res_data['success'])
+        self.assertFalse(len(res_data['questions']))
+
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
